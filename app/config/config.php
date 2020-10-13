acdd360b9a8813252297d44c6df3c0903bb5da3e
Pernilla Sterner <psterner@hotmail.se>
2020-10-13 14:34:09 +0200

    Create routing & connect to DB

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', '_DB_HOST_');
+  define('DB_USER', '_DB_USER_');
+  define('DB_PASS', '_DB_PASS_');
+  define('DB_NAME', '_DB_NAME_');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', '_URLROOT_');
+  // Site Name
+  define('SITENAME', '_SITENAME_');
+  // App Version
+  define('APPVERSION', '1.0.0');
